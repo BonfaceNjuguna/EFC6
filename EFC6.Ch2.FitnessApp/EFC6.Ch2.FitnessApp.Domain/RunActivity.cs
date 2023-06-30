36be810b55eeceb6985920456461fab0449bec7d
Bonface Njuguna <bonfacenjuguna13@gmail.com>
2023-06-30 18:43:16 +0300

    string nullable

--- EFC6.Ch2.FitnessApp/EFC6.Ch2.FitnessApp.Domain/RunActivity.cs
+++ EFC6.Ch2.FitnessApp/EFC6.Ch2.FitnessApp.Domain/RunActivity.cs
@@ -3,7 +3,7 @@
     public class RunActivity
     {
         public int id { get; set; }
-        public string name { get; set; }
+        public string? name { get; set; }
         public double distance { get; set; }
     }
 }
