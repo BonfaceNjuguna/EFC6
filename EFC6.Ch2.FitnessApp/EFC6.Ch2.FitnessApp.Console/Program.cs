42910783ea8e557b52d99528a15ca7ee20204dd7
Bonface Njuguna <bonfacenjuguna13@gmail.com>
2023-06-30 18:41:18 +0300

    added fitness console, data and domain

---
+++ EFC6.Ch2.FitnessApp/EFC6.Ch2.FitnessApp.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EFC6.Ch2.FitnessApp.Domain;
+
+
+FitnessAppContext _fitnessContext = new FitnessAppContext();
+
+AddRunActivity();
+
+void AddRunActivity()
+{
+    _fitnessContext.runActivity.Add(new RunActivity { id = 1, name = "Bonface", distance = 11.0});
+}
---
+++ EFC6.Ch2.FitnessApp/EFC6.Ch2.FitnessApp.Data/FitnessAppContext.cs
@@ -0,0 +1,13 @@
+﻿using EFC6.Ch2.FitnessApp.Domain;
+using Microsoft.EntityFrameworkCore;
+
+public class FitnessAppContext : DbContext
+{
+	public DbSet<RunActivity> runActivity { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server = localhost\\SQLEXPRESS; Database=FitnessDb; Trusted_Connection = True;"
+            );
+    }
+}
